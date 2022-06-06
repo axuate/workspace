da7fb1e647f8fb4bb7c9b9d7b75dcfb038d20df3
yhaering <mail@yhaering.de>
2022-06-06 14:00:32 +0200

    fix(eslint-config): missing parserOptions.project configuration

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -15,6 +15,9 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking'
       ],
+      parserOptions: {
+        project: './tsconfig.json'
+      },
       parser: '@typescript-eslint/parser',
       plugins: ['@typescript-eslint']
     },
