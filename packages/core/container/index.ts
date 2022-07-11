ddd33b6318c0edf20d17963250a5e90cffe5b9aa
yhaering <mail@yhaering.de>
2022-07-11 10:26:48 +0200

    fix(container): added missing exports

--- packages/core/container/index.ts
+++ packages/core/container/index.ts
@@ -1,3 +1,7 @@
 export * from './src/Container';
 export * from './src/resolver/classResolver';
 export * from './src/constants/reflection';
+export * from './src/decorators/Injectable';
+export * from './src/decorators/Inject';
+export * from './src/entities/Constructor';
+export * from './src/entities/Resolver';
